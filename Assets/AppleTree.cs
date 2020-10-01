a3a1a3a8bbd00270d38a4bf5e344e765c6e50c3d
Jakub Flaczyk <38633099+abuk74@users.noreply.github.com>
2020-10-01 12:05:45 +0200

    1st commit

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+    void Update()
+    {
+        //Basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //Zmiana kierunku poruszania się
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //left
+        }
+    }
+    void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+}
